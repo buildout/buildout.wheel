b88e791cb591f48d9822c65283e7c03bb41a3a7d
Leonardo Rochael Almeida <leorochael@gmail.com>
2017-02-20 05:08:33 -0300

    More surgical monkey-patch into setuptools
    Patch only `distros_for_location` instead of both
    `interpret_distro_name` and `EXTENSIONS`

--- src/buildout/wheel/__init__.py
+++ src/buildout/wheel/__init__.py
@@ -3,44 +3,67 @@ import humpty
 import os
 import pkg_resources
 import setuptools.package_index
-from six.moves.urllib import parse
 import wheel.install
+import wheel.decorator
 import pip.pep425tags
 import zc.buildout.easy_install
 
-orig_interpret_distro_name = setuptools.package_index.interpret_distro_name
+orig_distros_for_location = setuptools.package_index.distros_for_location
 original_wheel_to_egg = zc.buildout.easy_install.wheel_to_egg
 
 wheel_supported = pip.pep425tags.get_supported
+class WheelFile(wheel.install.WheelFile):
 
-def interpret_distro_name(
-    location, basename, metadata, py_version=None,
-    precedence=pkg_resources.SOURCE_DIST,
-    platform=None
-    ):
-    if '.whl' in location:
-        path = parse.urlparse(location).path
+    def __init__(self, *args, **kwargs):
+        # use pip's notion of supported tags, not wheel's:
+        kwargs.setdefault('context', pip.pep425tags.get_supported)
+        super(WheelFile, self).__init__(*args, **kwargs)
+
+    @wheel.decorator.reify
+    def is_platform_specific(self):
+        return (self.parsed_filename.group('abi') != 'none' or
+                self.parsed_filename.group('plat') != 'any')
+
+
+    @wheel.decorator.reify
+    def egg_platform(self):
+        if self.compatible and self.is_platform_specific:
+            # pretend we're a perfect match to avoid issues
+            # TODO: is this really necessary for buildout? Will buildout ignore
+            # manylinux1_x86_64 eggs in it's own eggs directory?
+            return pkg_resources.get_build_platform()
+        else:
+            plat = self.parsed_filename.group('plat')
+            return plat if plat != 'any' else None
+
+    def distribution(self, location=None, metadata=None,
+                     class_=pkg_resources.Distribution):
+        parsed = self.parsed_filename.groupdict()
+        return class_(
+            location=location if location is not None else self.filename,
+            metadata=metadata,
+            project_name=parsed['name'],
+            version=parsed['ver'],
+            platform=self.egg_platform,
+        )
+
+def distros_for_location(location, basename, metadata=None):
+    """Yield egg or source distribution objects based on basename
+
+    Here we override to give wheels a chance"""
+    if basename.endswith('.whl'):
         try:
-            wf = wheel.install.WheelFile(path, context=wheel_supported)
+            wf = WheelFile(basename)
         except wheel.install.BadWheelFile:
             pass
         else:
             if wf.compatible:
-                # It's a match. Treat it as a source
+                # It's a match. Treat it as an egg
                 # distro. Buildout will sort it out.
-                assert wf.distinfo_name.endswith('.dist-info')
-                return orig_interpret_distro_name(
-                    location, wf.distinfo_name[:-10], metadata,
-                    py_version=py_version,
-                    # EGG_DIST since we want wheels to be prefered over source,
-                    # and we'll turn them into eggs anyway:
-                    precedence=pkg_resources.EGG_DIST,
-                    platform=platform)
-            else:
-                # Not a match, short circuit:
-                return ()
-    return orig_interpret_distro_name(
-        location, basename, metadata, py_version, precedence, platform)
+                return [wf.distribution(location, metadata)]
+        # Not a match, short circuit:
+        return ()
+    return orig_distros_for_location(location, basename, metadata=metadata)
 
 def wheel_to_egg(dist, tmp):
     writer = humpty.EggWriter(dist.location)
@@ -50,7 +73,6 @@ def wheel_to_egg(dist, tmp):
         os.path.join(tmp, egg_name), egg_name)
 
 def load(buildout):
-    setuptools.package_index.EXTENSIONS.append('.whl')
-    setuptools.package_index.interpret_distro_name = interpret_distro_name
     distlib.wheel.COMPATIBLE_TAGS = set(wheel_supported())
+    setuptools.package_index.distros_for_location = distros_for_location
     zc.buildout.easy_install.wheel_to_egg = wheel_to_egg
