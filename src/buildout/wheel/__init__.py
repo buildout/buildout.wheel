a334b83ac4b28d6038b7fd99b64997e25c47ac6b
Leonardo Rochael Almeida <leorochael@gmail.com>
2017-02-20 17:05:34 -0300

    Forget laying eggs from wheels: use the wheels
    Unpack wheels directly as directories inside `eggs-directory`
    
    Override `zc.buildout.easy_install.Installer` to feed the wheels inside
    `eggs-directory` to `pkg_resources.Environment`.
    
    That's enough for `pkg_resources` to read all the metadata.
    
    Too bad... I liked the `lay_egg_into()` method name...

--- src/buildout/wheel/__init__.py
+++ src/buildout/wheel/__init__.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import os.path
 import shutil
@@ -11,32 +12,58 @@ import pip.pep425tags
 import zc.buildout.easy_install
 import distutils.command.install
 
+
 NAMESPACE_STUB_PATH = os.path.join(os.path.dirname(__file__),
                                    'namespace_stub.py')
 assert os.path.isfile(NAMESPACE_STUB_PATH)
 
+
 orig_distros_for_location = setuptools.package_index.distros_for_location
 original_wheel_to_egg = zc.buildout.easy_install.wheel_to_egg
+orig_Installer = zc.buildout.easy_install.Installer
 
-class SelfDestructingDistro(pkg_resources.Distribution):
+
+class Installer(orig_Installer):
+    """Installer class that enriches the environment with all the wheels in the
+    egg directory, since `Environment` can't find them on its own like it can
+    find eggs.
+    """
+
+    def __init__(self, *args, **kwargs):
+        orig_Installer.__init__(self, *args, **kwargs)
+        dest = self._dest
+        # include unpacked wheel directories in path after `self._dest`:
+        wheeldirs = [
+            wheeldir for wheeldir in glob.glob(os.path.join(dest, '*.whl'))
+            if os.path.isdir(wheeldir)
+        ]
+        path = self._path
+        path[1:1] = wheeldirs
+        # Sorry to throw away all the previous hard work creating self._env
+        # but it didn't take the wheel directories into account:
+        self._env = pkg_resources.Environment(path)
+
+
+class SelfDestructingDistro(pkg_resources.DistInfoDistribution):
     """Since the location for creating the egg is inside the download directory
     which could be the download cache, this wheel was created in a tempdir
-    inside it and we must take it down to avoid leaving garbage around.
+    inside it and we must clean up the tempdir to avoid leaving garbage behind.
     """
     def __del__(self):
         shutil.rmtree(os.path.dirname(self.location))
 
-class WheelFile(wheel.install.WheelFile):
+
+class WheelDir(wheel.install.WheelFile):
 
     def __init__(self, *args, **kwargs):
         # use pip's notion of supported tags, not wheel's:
         kwargs.setdefault('context', pip.pep425tags.get_supported)
-        super(WheelFile, self).__init__(*args, **kwargs)
+        super(WheelDir, self).__init__(*args, **kwargs)
 
-    def lay_egg_into(self, target):
+    def get_temp_dist(self):
         join = os.path.join
-        dest = tempfile.mkdtemp(dir=target)
-        location = join(dest, self.egg_name)
+        target = tempfile.mkdtemp()
+        location = join(target, os.path.basename(self.filename))
         # drop everything inside the metadata directory, keyed by section name,
         # except the code itself:
         overrides = {
@@ -45,55 +72,51 @@ class WheelFile(wheel.install.WheelFile):
             for key in distutils.command.install.SCHEME_KEYS
         }
         self.install(overrides=overrides)
-        # pkg_resources special-cases directories in sys.path with `.egg`
-        # extension and expect their metadata directory to be called EGG-INFO
-        # instead of the `[name]-[version].dist-info` directory of wheels.
-        egg_info_location = join(location, 'EGG-INFO')
-        os.rename(join(location, self.distinfo_name), egg_info_location)
-        # See docstring of SelfDestructingDistro above for why it's needed:
-        metadata = pkg_resources.PathMetadata(location, egg_info_location)
-        egg = self.distribution(location, metadata=metadata,
-                                class_=SelfDestructingDistro)
-        # Fix namespaces with missing __init__
+
+        metadata = pkg_resources.PathMetadata(
+            location, join(location, self.distinfo_name)
+        )
+        # Fix namespaces missing __init__ for Python 2 since we're not feeding
+        # the wheel dirs to `site.addsitedir()` and so will ignore .pth files
+        # in the wheel dirs.
         if (sys.version_info < (3, 3) and
-                egg.has_metadata('namespace_packages.txt')):
-            for namespace in egg.get_metadata_lines('namespace_packages.txt'):
+                metadata.has_metadata('namespace_packages.txt')):
+            for namespace in metadata.get_metadata_lines('namespace_packages.txt'):
                 __init__filename = namespace.split('.') + ['__init__.py']
                 dest = join(location, *__init__filename)
                 if not os.path.exists(dest):
                     shutil.copyfile(NAMESPACE_STUB_PATH, dest)
-        return egg
+        # See docstring of SelfDestructingDistro above for why it's needed:
+        dist = self.distribution(location, metadata=metadata,
+                                 class_=SelfDestructingDistro)
+        return dist
 
     @wheel.decorator.reify
-    def egg_name(self):
-        return self.distribution().egg_name() + '.egg'
-
-    @wheel.decorator.reify
-    def is_platform_specific(self):
-        return (self.parsed_filename.group('abi') != 'none' or
-                self.parsed_filename.group('plat') != 'any')
-
-    @wheel.decorator.reify
-    def egg_platform(self):
-        if self.compatible and self.is_platform_specific:
-            # pretend we're a perfect match to avoid issues
-            # TODO: is this really necessary for buildout? Will buildout ignore
-            # manylinux1_x86_64 eggs in it's own eggs directory?
-            return pkg_resources.get_build_platform()
-        else:
-            plat = self.parsed_filename.group('plat')
-            return plat if plat != 'any' else None
+    def distribution_info(self):
+        info = self.parsed_filename.groupdict()
+        if info['plat'] == 'any':
+            # Pure Python
+            info['plat'] = None
+        elif self.compatible:
+            # abi/platform specific, but compatible wheel. Pretend we're a
+            # perfect match to avoid setuptool ignoring the wheel since it
+            # doesn't understand, e.g. manylinux1.
+            info['plat'] = pkg_resources.get_build_platform()
+        return info
 
     def distribution(self, location=None, metadata=None,
-                     class_=pkg_resources.Distribution):
-        parsed = self.parsed_filename.groupdict()
+                     class_=pkg_resources.DistInfoDistribution):
+        info = self.distribution_info
         return class_(
             location=location if location is not None else self.filename,
             metadata=metadata,
-            project_name=parsed['name'],
-            version=parsed['ver'],
-            platform=self.egg_platform,
+            project_name=info['name'],
+            version=info['ver'],
+            platform=info['plat'],
+            # make sure buildout believes it's an egg with anoter extension:
+            precedence=pkg_resources.EGG_DIST,
         )
+
 
 def distros_for_location(location, basename, metadata=None):
     """Yield egg or source distribution objects based on basename
@@ -101,7 +124,7 @@ def distros_for_location(location, basename, metadata=None):
     Here we override to give wheels a chance"""
     if basename.endswith('.whl'):
         try:
-            wf = WheelFile(basename)
+            wf = WheelDir(basename)
         except wheel.install.BadWheelFile:
             pass
         else:
@@ -113,10 +136,15 @@ def distros_for_location(location, basename, metadata=None):
         return ()
     return orig_distros_for_location(location, basename, metadata=metadata)
 
+
 def wheel_to_egg(dist, tmp):
-    wf = WheelFile(dist.location)
-    return wf.lay_egg_into(tmp)
+    # Ignore `tmp`. It could be the download cache and using it for anything
+    # other than creating tempdirs as subdirectories risks collisions
+    # between buildout runs using a shared download cache.
+    return WheelDir(dist.location).get_temp_dist()
+
 
 def load(buildout):
     setuptools.package_index.distros_for_location = distros_for_location
+    zc.buildout.easy_install.Installer = Installer
     zc.buildout.easy_install.wheel_to_egg = wheel_to_egg
