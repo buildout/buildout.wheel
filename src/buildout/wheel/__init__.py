5724cc513bddcb5a991898be24e7fec820f52166
Leonardo Rochael Almeida <leorochael@gmail.com>
2017-02-23 17:59:24 -0300

    Look, Ma! No foreign subclasses!
    Stop subclassing `pkg_resources.Environment`.
    
    Just override `Installer._call_easy_install()` to unpack wheels
    inside `eggs-directory` such that `Installer` knows to find them.

--- src/buildout/wheel/__init__.py
+++ src/buildout/wheel/__init__.py
@@ -1,14 +1,11 @@
-import glob
 import logging
 import os
 import os.path
 import shutil
 import sys
-import tempfile
 import pkg_resources
 import setuptools.package_index
 import wheel.install
-import wheel.decorator
 import pip.pep425tags
 import zc.buildout.easy_install
 import distutils.command.install
@@ -26,74 +23,16 @@ original_wheel_to_egg = zc.buildout.easy_install.wheel_to_egg
 orig_Installer = zc.buildout.easy_install.Installer
 
 
-def _get_dest_dist_paths(dest):
-    p = os.path
-    eggs = glob.glob(p.join(dest, '*.egg'))
-    dists = [p.dirname(dist_info) for dist_info in
-             glob.glob(p.join(dest, '*', '*.dist-info'))]
-    # sort them like pkg_resources.find_on_path() would
-    return pkg_resources._by_version_descending(set(eggs + dists))
-
-
-class Environment(pkg_resources.Environment):
-    """
-    An pkg_resources.Environment specialiation that knows how to scan all
-    dists under `eggs-directory`, regardless of whether they're actual eggs.
-    """
-
-    def __init__(self, search_path, dest):
-        self.__dest = pkg_resources.normalize_path(dest)
-        pkg_resources.Environment.__init__(self, search_path)
-
-    def scan(self, search_path=None):
-        # replace `self.__dest` (the `eggs-directory`) inside `search_path`
-        # with all distributions (egg or otherwise) located in `self.__dest`
-        # since `pkg_resources.Environment` can detect eggs in `self.__dest`,
-        # but not other kinds of dists:
-        if (search_path and
-                pkg_resources.normalize_path(search_path[0]) == self.__dest):
-            search_path[0:1] = _get_dest_dist_paths(self.__dest)
-        return pkg_resources.Environment.scan(self, search_path)
-
-    def add(self, dist):
-        # pkg_resources.find_on_path(), used by self.scan() via
-        # find_distributions(), creates all non-.egg dists as
-        # precedence=DEVELOP_DIST.
-        # Fix precedence to EGG_DIST of all packages under `self.__dest`
-        if dist.location.startswith(self.__dest + '/'):
-            dist.precedence = pkg_resources.EGG_DIST
-        return pkg_resources.Environment.add(self, dist)
-
-
 class Installer(orig_Installer):
     """
-    zc.buildout.easy_install.Installer class that feeds all dists in
-    `eggs-directory` to `pkg_resources.Environment`, not just the eggs that
-    `Environment` would auto-detect, and knows how to install `wheels`.
+    zc.buildout.easy_install.Installer class that knows how to install `wheels`
     """
 
-    def __init__(self, *args, **kwargs):
-        orig_Installer.__init__(self, *args, **kwargs)
-        # Sorry to throw away all the previous hard work creating self._env
-        # but we need to replace it with our special Environment.
-        self._env = Environment(self._path, dest=self._dest)
-
-    def _call_easy_install(self, spec, ws, dest, dist):
+    def _call_easy_install(self, spec, dest):
         if not spec.endswith('.whl'):
-            return orig_Installer._call_easy_install(spec, ws, dest, dist)
-        tmp = tempfile.mkdtemp(dir=dest)
-        try:
-            # this causes the dist tree to be written twice in tempdirs inside
-            # `dest`, but the alternative would be to trick
-            # `setuptools.archive_util.unpack_archive` into unpacking wheels
-            # which requirs dealing with
-            # `setuptools.archive_util.unpack_archive.default_filter`.
-            dist = WheelInstaller(spec).install_into(tmp)
-            move = zc.buildout.easy_install._move_to_eggs_dir_and_compile
-            newloc = move(dist, dest)
-            return pkg_resources.Environment([newloc])[dist.project_name]
-        finally:
-            shutil.rmtree(tmp)
+            orig_Installer._call_easy_install(spec, dest)
+        location = WheelInstaller(spec).install_into(dest)
+        return [location]
 
 
 class WheelInstaller(object):
@@ -102,11 +41,11 @@ class WheelInstaller(object):
 
      * return a DistInfoDistribution that buildout can install
 
-     * install the wheel into a directory and return the respective
-       `DistInfoDistribution`.
+     * install the wheel into a directory to be added to `sys.path`
     """
 
     _dist_extension = '.dist'
+    wheel = None
 
     def __init__(self, location):
         # use pip's notion of supported tags, not wheel's:
@@ -114,11 +53,11 @@ class WheelInstaller(object):
         try:
             self.wheel = wheel.install.WheelFile(location, context=context)
         except wheel.install.BadWheelFile:
-            self.wheel = False
+            pass
 
     @property
     def compatible(self):
-        return self.wheel and self.wheel.compatible
+        return self.wheel is not None and self.wheel.compatible
 
     def install_into(self, target):
         """
@@ -136,32 +75,25 @@ class WheelInstaller(object):
             for key in distutils.command.install.SCHEME_KEYS
         }
         self.wheel.install(overrides=overrides)
-
-        metadata = pkg_resources.PathMetadata(
-            location, os.path.join(location, self.wheel.distinfo_name)
-        )
         # Fix namespaces missing __init__ for Python 2 since we're not feeding
         # the wheel dirs to `site.addsitedir()` and so Python will ignore .pth
         # files in the wheel dirs.
+        metadata = pkg_resources.PathMetadata(
+            location, os.path.join(location, self.wheel.distinfo_name)
+        )
+        dist = self.distribution(location, metadata=metadata)
         if (sys.version_info < (3, 3) and
                 metadata.has_metadata('namespace_packages.txt')):
-            self._plant_namespace_declarations(location, metadata)
-        # The installed distribution is egg-like to please buildout
-        return self.distribution(location, metadata=metadata,
-                                 precedence=pkg_resources.EGG_DIST)
+            self._plant_namespace_declarations(dist)
+        return dist
 
     @staticmethod
-    def _plant_namespace_declarations(root, metadata):
-        base = [root]
-        init = ['__init__.py']
-        for namespace in metadata.get_metadata_lines('namespace_packages.txt'):
-            __init__filename = os.path.join(*(
-                base + namespace.split('.') + init
-            ))
+    def _plant_namespace_declarations(dist):
+        paths = zc.buildout.easy_install.get_namespace_package_paths(dist)
+        for __init__filename in paths:
             if not os.path.exists(__init__filename):
                 shutil.copyfile(NAMESPACE_STUB_PATH, __init__filename)
 
-    @wheel.decorator.reify
     def distribution_info(self):
         """
         Parsed info from the wheel name, with a setuptools compatible platform
@@ -182,18 +114,18 @@ class WheelInstaller(object):
             info['plat'] = 'incompatible'
         return info
 
-    def distribution(self, location, metadata=None,
-                     precedence=pkg_resources.BINARY_DIST):
-        info = self.distribution_info
+    def distribution(self, location, metadata=None):
+        """ Get DistInfoDistribution for wheel """
+        info = self.distribution_info()
         return pkg_resources.DistInfoDistribution(
             location=location,
             metadata=metadata,
             project_name=info['name'],
             version=info['ver'],
             platform=info['plat'],
-            # trick buildout into believing this dist is an egg with anoter
-            # extension so it copies the dist into `eggs-directory`:
-            precedence=precedence,
+            # trick buildout into believing this dist is a BINARY_DIS
+            # so it invokes our `_call_easy_install` override:
+            precedence=pkg_resources.BINARY_DIST,
         )
 
 
